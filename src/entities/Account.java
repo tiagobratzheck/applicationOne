59f106f5f88843f3604697ae627fd50ddbd66f59
tiagobratzheck <tiagobratzheck@hotmail.com>
2018-11-07 19:27:31 -0200

    Redução de linhas

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -11,24 +11,20 @@ public class Account {
 	public Account(int number, String holder) {
 		this.number = number;
 		this.holder = holder;
-
 	}
 
 	public Account(int number, String holder, double initialDeposit) {
 		this.number = number;
 		this.holder = holder;
 		deposit(initialDeposit);
-
 	}
 
 	public int getNumber() {
 		return number;
-
 	}
 
 	public String getHolder() {
 		return holder;
-
 	}
 
 	public void setHolder(String holder) {
@@ -38,34 +34,26 @@ public class Account {
 
 	public double getBalance() {
 		return balance;
-
 	}
 
 	public void deposit(double amount) {
 		balance += amount;
-
 	}	
 
 	public void withdraw(double amount) {
 		balance -= amount + FEE;
-
 	}	
 	
-	public void investing(int time, double fee, double amount) {
-		
-		double initial = amount;
-		
-		for(int i = 1; i <= time; i++) {
-			
+	public void investing(int time, double fee, double amount) {		
+		double initial = amount;		
+		for(int i = 1; i <= time; i++) {			
 			amount += (amount * fee);
 		}
-		showResult(initial, amount);
-		
+		showResult(initial, amount);		
 	}
 		
 	private void showResult(double initial, double amount) {
-		System.out.printf("The result of the investment is: $  %.2f%n of the initial value: $  %.2f%n", amount, initial);
-		
+		System.out.printf("The result of the investment is: $  %.2f%n of the initial value: $  %.2f%n", amount, initial);		
 	}
 	
 	public String toString() {
@@ -76,7 +64,5 @@ public class Account {
 				+ ", Balance: $ "
 				+ String.format("%.2f", balance);
 	}
-
-	
 
 }
